1d48424093780e02c1666da17eb17421cb42ede5
ZenT3600 <matteo.leggio@tiscali.it>
2022-11-21 12:36:59 +0100

    Added ability to create cables

--- floorplanner.py
+++ floorplanner.py
@@ -5,6 +5,15 @@ from typing import List
 from PIL import Image, ImageDraw
 
 
+class Cable:
+    def __init__(self, room_uid: str, size: str, start: List[int], end: List[int], vertical: bool):
+        self.room_uid = int(room_uid)
+        self.size = int(size)
+        self.start = start
+        self.end = end
+        self.is_vertical = vertical
+
+
 class Box:
     def __init__(self, room_uid: str, name: str, anchor: List[int], color: List[int]):
         self.room_uid = int(room_uid)
@@ -14,12 +23,13 @@ class Box:
 
 
 class Room:
-    def __init__(self, uid: str, width: str, height: str, anchor: List[int], boxes: List[Box]):
+    def __init__(self, uid: str, width: str, height: str, anchor: List[int], boxes: List[Box], cables: List[Cable]):
         self.uid = int(uid)
         self.width = int(width)
         self.height = int(height)
         self.anchor = anchor
         self.boxes = boxes
+        self.cables = cables
 
 
 def parseRooms(src):
@@ -30,12 +40,13 @@ def parseRooms(src):
     for line in lines:
         if line.startswith("new room"):
             if not all([keyword in line for keyword in ["id", "width", "height", "anchor"]]):
-                assert False, "Invalid room creation syntax"
+                assert False, "Invalid room creation syntax: \"" + line + "\""
             keywords = line.split(" ")[2::2]
             values = line.split(" ")[3::2]
             zipped = {k: v for k, v in zip(keywords, values)}
             BOXES = parseBoxesForRoom(src, zipped["id"])
-            ROOMS.append(Room(zipped["id"], zipped["width"], zipped["height"], [int(n) for n in zipped["anchor"].split(",")], BOXES))
+            CABLES = parseCablesForRoom(src, zipped["id"])
+            ROOMS.append(Room(zipped["id"], zipped["width"], zipped["height"], [int(n) for n in zipped["anchor"].split(",")], BOXES, CABLES))
 
     return ROOMS
 
@@ -47,7 +58,7 @@ def parseBoxesForRoom(src, room):
     for line in lines:
         if line.startswith("new box"):
             if not all([keyword in line for keyword in ["room", "name", "anchor"]]):
-                assert False, "Invalid box creation syntax"
+                assert False, "Invalid box creation syntax: \"" + line + "\""
             keywords = line.split(" ")[2::2]
             values = line.split(" ")[3::2]
             zipped = {k: v for k, v in zip(keywords, values)}
@@ -60,41 +71,79 @@ def parseBoxesForRoom(src, room):
 
     return BOXES
 
+def parseCablesForRoom(src, room):
+    with open(src, "r") as f:
+        lines = [line.strip() for line in f.readlines() if line]
+
+    CABLES = []
+    for line in lines:
+        if line.startswith("new cable"):
+            if not all([keyword in line for keyword in ["room", "type", "size", "from", "to"]]):
+                assert False, "Invalid cable creation syntax: \"" + line + "\""
+            keywords = line.split(" ")[2::2]
+            values = line.split(" ")[3::2]
+            zipped = {k: v for k, v in zip(keywords, values)}
+            if zipped["type"] not in ["V", "H"]:
+                assert False, "Cable type can only be V or H"
+            if zipped["type"] == "V":
+                if [int(n) for n in zipped["from"].split(",")][0] != [int(n) for n in zipped["to"].split(",")][0]:
+                    assert False, "Malformed vertical cable"
+            else:
+                if [int(n) for n in zipped["from"].split(",")][1] != [int(n) for n in zipped["to"].split(",")][1]:
+                    assert False, "Malformed horizontal cable"
+            if int(zipped["size"]) <= 0:
+                assert False, "Invalid cable size"
+            if int(zipped["room"]) != int(room):
+                continue
+            CABLES.append(Cable(zipped["room"], zipped["size"], [int(n) for n in zipped["from"].split(",")], [int(n) for n in zipped["to"].split(",")], zipped["type"] == "V"))
+
+    return CABLES
+
+
 def drawRooms(rooms):
     for room in rooms:
-        # inverted because of PIL's coordinate system
-        height = 32 * 9 * room.width
-        width  = 32 * 9 * room.height
-        
-        step_count = height / 32
-        image = Image.new(mode="RGB", size=(height, width), color=(255,) * 3)
-        draw = ImageDraw.Draw(image)
-        y_start = 0
-        y_end = image.height
-        step_size = int(image.width / step_count)
-        for x in range(0, image.width, step_size):
-            line = ((x, y_start), (x, y_end))
-            draw.line(line, fill=(192,) * 3)
-        x_start = 0
-        x_end = image.width
-        for y in range(0, image.height, step_size):
-            line = ((x_start, y), (x_end, y))
-            draw.line(line, fill=(192,) * 3)
+        drawRoom(room)
 
-        draw.line(((0, 0),(height, 0)), fill=0, width=3)
-        draw.line(((0, 0),(0, width)), fill=0, width=3)
-        draw.line(((height, width),(height, 0)), fill=0, width=3)
-        draw.line(((height, width),(0, width)), fill=0, width=3)
-        del draw
+def drawRoom(room):
+    # inverted because of PIL's coordinate system
+    height = 32 * 9 * room.width
+    width  = 32 * 9 * room.height
 
-        drawRoom(room, image)
-
-def drawRoom(room, image):
+    image = Image.new(mode="RGB", size=(height, width), color=(255,) * 3)
     draw = ImageDraw.Draw(image)
+        
+    step_count = height / 32
+    image = Image.new(mode="RGB", size=(height, width), color=(255,) * 3)
+    draw = ImageDraw.Draw(image)
+    y_start = 0
+    y_end = image.height
+    step_size = int(image.width / step_count)
+    for x in range(0, image.width, step_size):
+        line = ((x, y_start), (x, y_end))
+        draw.line(line, fill=(192,) * 3)
+    x_start = 0
+    x_end = image.width
+    for y in range(0, image.height, step_size):
+        line = ((x_start, y), (x_end, y))
+        draw.line(line, fill=(192,) * 3)
+    draw.line(((0, 0),(height, 0)), fill=0, width=3)
+    draw.line(((0, 0),(0, width)), fill=0, width=3)
+    draw.line(((height, width),(height, 0)), fill=0, width=3)
+    draw.line(((height, width),(0, width)), fill=0, width=3)
+
     for box in room.boxes:
         a = box.anchor
         draw.rectangle((a[0] * 32, a[1] * 32, a[0] * 32 + 32, a[1] * 32 + 32), fill=tuple(box.color), outline=(0))
         draw.text((a[0] * 32 + 4, a[1] * 32 + 12), box.name, (255,) * 3)
+    for cable in room.cables:
+        s = cable.start
+        e = cable.end
+        if cable.is_vertical:
+            for i in range(1, cable.size + 1):
+                draw.line(((s[0] * 32 + 4 * i, s[1] * 32 + 4), (e[0] * 32 + 4 * i, e[1] * 32 + 4)), fill=(255, 0, 0), width=1)
+        else:
+            for i in range(1, cable.size + 1):
+                draw.line(((s[0] * 32 + 4, s[1] * 32 + 4 * i), (e[0] * 32 + 4, e[1] * 32 + 4 * i)), fill=(255, 0, 0), width=1)
     image.save(str(room.uid) + ".png")
         
 def main():
